7332febb8bead6d1e4345c24f4ec03b30030f852
truonghuy2490 <huydocon1@gmail.com>
2023-10-08 23:11:15 +0700

    Initial commit

---
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/MeetMyLectureApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.meetMyLecturer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetMyLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetMyLectureApplication.class, args);
+	}
+
+}
