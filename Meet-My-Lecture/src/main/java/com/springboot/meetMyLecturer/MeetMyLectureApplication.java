756568e8c201461961e2613a6c84bd02178c8228
truonghuy2490 <huydocon1@gmail.com>
2023-10-18 11:24:52 +0700

    Swagger testing

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/MeetMyLectureApplication.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/MeetMyLectureApplication.java
@@ -1,7 +1,12 @@
 package com.springboot.meetMyLecturer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class MeetMyLectureApplication {
