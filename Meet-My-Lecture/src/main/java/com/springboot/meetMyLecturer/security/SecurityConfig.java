2d79e821360e82ed6465c2cbbbcb4e3a35afa2d8
truonghuy2490 <huydocon1@gmail.com>
2023-10-19 16:16:46 +0700

    config by http -> https

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/security/SecurityConfig.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedOrigins(Collections.singletonList("https://localhost:3000"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
