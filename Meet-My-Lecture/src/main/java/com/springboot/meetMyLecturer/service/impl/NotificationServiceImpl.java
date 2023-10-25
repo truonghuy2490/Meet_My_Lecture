960c8b87a499e46c2af1c7cc02020d650aed6026
truonghuy2490 <huydocon1@gmail.com>
2023-10-25 19:02:12 +0700

    Pageable: Request and Empty Slot

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/NotificationServiceImpl.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/NotificationServiceImpl.java
@@ -13,10 +13,7 @@ import com.springboot.meetMyLecturer.utils.NotificationType;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
+
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
