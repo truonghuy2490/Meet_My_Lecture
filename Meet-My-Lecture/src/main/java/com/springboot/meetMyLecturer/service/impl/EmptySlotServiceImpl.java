c0e21eadcd4871a75476794c33f733ecc445e3a4
truonghuy2490 <huydocon1@gmail.com>
2023-10-26 22:25:47 +0700

    Scrip SQL

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/EmptySlotServiceImpl.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/EmptySlotServiceImpl.java
@@ -243,7 +243,7 @@ public class EmptySlotServiceImpl implements EmptySlotService {
 
 
     // check if slot is expired
-    @Scheduled(cron = "0 0 6,12 * * ?")
+   /* @Scheduled(cron = "0 0 6,12 * * ?")
     public void checkIfEmptySlotIsExpired() {
 
         java.sql.Date dateNow = java.sql.Date.valueOf(LocalDate.now());
@@ -272,7 +272,7 @@ public class EmptySlotServiceImpl implements EmptySlotService {
         }
 
 
-    }
+    }*/
 
     public static LocalTime addTimes(LocalTime time1, LocalTime time2) {
         Duration duration1 = Duration.between(LocalTime.MIDNIGHT, time1);
--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/WeeklyEmptySlotServiceImpl.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/service/impl/WeeklyEmptySlotServiceImpl.java
@@ -5,7 +5,7 @@ import com.springboot.meetMyLecturer.ResponseDTO.WeeklyEmptySlotResponseForAdmin
 import com.springboot.meetMyLecturer.constant.Constant;
 import com.springboot.meetMyLecturer.entity.EmptySlot;
 import com.springboot.meetMyLecturer.entity.User;
-import com.springboot.meetMyLecturer.ResponseDTO.WeeklyEmptySlotResponseDTO;
+
 import com.springboot.meetMyLecturer.entity.Semester;
 import com.springboot.meetMyLecturer.entity.WeeklyEmptySlot;
 import com.springboot.meetMyLecturer.exception.ResourceNotFoundException;
@@ -14,7 +14,7 @@ import com.springboot.meetMyLecturer.repository.EmptySlotRepository;
 import com.springboot.meetMyLecturer.repository.SemesterRepository;
 import com.springboot.meetMyLecturer.repository.UserRepository;
 import com.springboot.meetMyLecturer.repository.WeeklySlotRepository;
-import com.springboot.meetMyLecturer.service.SemesterService;
+
 import com.springboot.meetMyLecturer.service.WeeklyEmptySlotService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,27 +34,26 @@ public class WeeklyEmptySlotServiceImpl implements WeeklyEmptySlotService {
     @Autowired
     ModelMapper mapper;
     @Autowired
-    SemesterService semesterService;
-    @Autowired
     SemesterRepository semesterRepository;
     @Autowired
     EmptySlotRepository emptySlotRepository;
-    @Override
-    public List<WeeklyDTO> getAllWeekly() {
-        List<WeeklyEmptySlot> weeklyEmptySlots = weeklySlotRepository.findAll();
-        List<WeeklyDTO> weeklyDTOList = weeklyEmptySlots.stream().map(
-                weeklyEmptySlot -> mapper.map(weeklyEmptySlot,WeeklyDTO.class)
-        ).collect(Collectors.toList());
-        return weeklyDTOList;
-    }
+
+//    @Override
+//    public List<WeeklyDTO> getAllWeekly() {
+//        List<WeeklyEmptySlot> weeklyEmptySlots = weeklySlotRepository.findAll();
+//        List<WeeklyDTO> weeklyDTOList = weeklyEmptySlots.stream().map(
+//                weeklyEmptySlot -> mapper.map(weeklyEmptySlot,WeeklyDTO.class)
+//        ).collect(Collectors.toList());
+//        return weeklyDTOList;
+//    }
 
     // test create weekly before automation adding
-    @Override
-    public WeeklyDTO createWeekly(WeeklyDTO weeklyDTO) {
-        WeeklyEmptySlot weeklyEmptySlot = mapper.map(weeklyDTO, WeeklyEmptySlot.class);
-        weeklySlotRepository.save(weeklyEmptySlot);
-        return weeklyDTO;
-    }
+//    @Override
+//    public WeeklyDTO createWeekly(WeeklyDTO weeklyDTO) {
+//        WeeklyEmptySlot weeklyEmptySlot = mapper.map(weeklyDTO, WeeklyEmptySlot.class);
+//        weeklySlotRepository.save(weeklyEmptySlot);
+//        return weeklyDTO;
+//    }
 
     @Override
     public WeeklyDTO createWeeklyByDateAt(Date date) {
@@ -191,51 +190,5 @@ public class WeeklyEmptySlotServiceImpl implements WeeklyEmptySlotService {
         ).collect(Collectors.toList());
     }
 
-    @Override
-    public WeeklyDTO insertIntoWeeklyByDateAt(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY); // Define your preferred first day of the week
 
-        // Calculate the end of the week by adding 6 days
-        Calendar endOfWeekCalendar = (Calendar) calendar.clone();
-        endOfWeekCalendar.add(Calendar.DATE, 6);
-
-        // Extract the start and end dates
-        Date startDate = calendar.getTime();
-        Date endDate = endOfWeekCalendar.getTime();
-
-        // Convert java.util.Date to java.sql.Date
-        java.sql.Date sqlStartDate = new java.sql.Date(startDate.getTime());
-        java.sql.Date sqlEndDate = new java.sql.Date(endDate.getTime());
-
-        // find weekly exist first date of week in DB
-        WeeklyEmptySlot checkWeeklyDate = weeklySlotRepository.findWeeklyEmptySlotByFirstDayOfWeek(sqlStartDate);
-
-        if (checkWeeklyDate == null) {
-            // Check if exist 1 weekly has day start
-            if (!startDate.equals(checkWeeklyDate)) {
-                // if it does not exist, create a WeeklyDTO object and set the start and end dates as java.sql.Date
-                // if create weekly NEED to add into Semester !!
-                WeeklyEmptySlot weeklyEmptySlot = new WeeklyEmptySlot();
-                weeklyEmptySlot.setFirstDayOfWeek(sqlStartDate);
-                weeklyEmptySlot.setLastDayOfWeek(sqlEndDate);
-
-                // insert into semester - If there are avaiable semester
-                Semester semester = semesterRepository.findSemesterByDateStart(sqlStartDate);
-                if (semester == null) {
-                    throw new RuntimeException("This semester are not avaiable to booking");
-                }
-                weeklyEmptySlot.setSemester(semester);
-
-                // save to DB
-                weeklySlotRepository.save(weeklyEmptySlot);
-
-                return mapper.map(weeklyEmptySlot, WeeklyDTO.class);
-            }
-        }
-        WeeklyEmptySlot weeklyEmptySlot = weeklySlotRepository.findWeeklyEmptySlotByFirstDayOfWeek(sqlStartDate);
-        return mapper.map(weeklyEmptySlot, WeeklyDTO.class);
-
-    }
 }
