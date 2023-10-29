7ac89b8c505fbec40bd5cfcc80a732dede616034
truonghuy2490 <huydocon1@gmail.com>
2023-10-29 16:10:13 +0700

    Fix room create slot error + update constanat

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/constant/Constant.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/constant/Constant.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Component;
 public class Constant {
 
     public static final String CLOSED = "CLOSED";
+
     public static final String OPEN = "OPEN";
     public static final String BANNED = "BANNED";
     public static final String LECTURER = "LECTURER";
     public static final String STUDENT = "STUDENT";
-
     public static String EMAIL;
     public static final String PRIVATE = "PRIVATE";
     public static final String APPROVED = "APPROVED";
     public static final String PENDING = "PENDING";
+    public static final String ACCEPTED = "ACCEPTED";
+    public static final String BOOK = "BOOK";
+    public static final String EXPIRED = "EXPIRED";
 
 }
