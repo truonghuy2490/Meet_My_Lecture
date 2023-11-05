c7075e793d4d4f3f199d5f9732e50549814c9f65
minnhat080903 <nhat892003@gmail.com>
2023-11-05 22:33:59 +0700

    05/11/2023 da xong spam

---
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/config/RequestConfig.java
@@ -0,0 +1,80 @@
+package com.springboot.meetMyLecturer.config;
+
+
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RequestConfig implements Filter {
+
+    private final LoadingCache<String, Bucket> requestCountsPerIpAddress;
+
+    public RequestConfig(){
+        super();
+        requestCountsPerIpAddress = Caffeine.newBuilder().
+                expireAfterWrite(10, TimeUnit.MINUTES).build(new CacheLoader<String, Bucket>() {
+                    public Bucket load(String key) {
+                        return Bucket.builder()
+                                .addLimit(Bandwidth.classic(2, Refill.intervally(1, Duration.ofMinutes(5))))
+                                .build();
+                    }
+                });
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        String clientIpAddress = getClientIP(httpServletRequest);
+
+        if(!IsRequestValid(clientIpAddress)){
+            httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    public String getClientIP(HttpServletRequest request) {
+        String header = request.getHeader("X-Forwarded-For");
+        if (header == null){
+            return request.getRemoteAddr();
+        }
+        return header.split(",")[0];
+    }
+
+    private boolean IsRequestValid(String ipAddress) {
+        return requestCountsPerIpAddress.get(ipAddress).tryConsume(1);
+    }
+
+    /*private void insertIntoCache(String ipAddress) {
+        requestCountsPerIpAddress.put(ipAddress,
+                Bucket.builder()
+                        .addLimit(Bandwidth.classic(2, Refill.intervally(1, Duration.ofMinutes(5)))) // set your request rate limit
+                        .build());
+    }*/
+}
---
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/filter/RequestFilter.java
@@ -0,0 +1,22 @@
+package com.springboot.meetMyLecturer.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class RequestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
