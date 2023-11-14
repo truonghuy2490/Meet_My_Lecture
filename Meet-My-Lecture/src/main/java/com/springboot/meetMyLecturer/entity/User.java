3f8c4713991a5ff408e5135775ff00ac9eb0d1af
minnhat080903 <nhat892003@gmail.com>
2023-11-15 01:12:22 +0700

    13/11/2023

--- Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/entity/User.java
+++ Meet-My-Lecture/src/main/java/com/springboot/meetMyLecturer/entity/User.java
@@ -40,12 +40,5 @@ public class User {
 
     private int absentCount;
 
-    /*@ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "lecturer_subject",
-            joinColumns = @JoinColumn(name = "lecturer_id"),
-            inverseJoinColumns = @JoinColumn(name = "subject_id")
-    )
-    private Set<Subject> subjectSet;*/
-
 
 }
